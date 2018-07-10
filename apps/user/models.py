d2acc873ea6d191450a236edbed08701e7f437a5
wg <564466548@qq.com>
2018-07-10 23:58:27 +0800

    wg

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class User(object):
+	user_id = models.IntegerField(primary_key=True,db_column='FId')
 	username = models.CharField( max_length= 20 )
 	password = models.CharField( max_length= 20 )
 	def __init__(self, arg):
--- ele_django/settings.py
+++ ele_django/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'ele_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'user',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'password',
     }
 }
 
