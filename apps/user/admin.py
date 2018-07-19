01f9594a86a5fb91da9de06411b3da348f880b15
王刚 <564466548@qq.com>
2018-07-19 17:39:24 +0800

    'a'

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-
+from django.utils.html import format_html
 from .models import User
+
 # Register your models here.
+# class adminUser(admin.ModelAdmin):
+# 	list_display = ('user_id', 'username', 'password', 'size' )
+# 	def 修改名字(self,obj):
+# 		return format_html('<input type="text" value="{a}"/>',a=obj)
+# 		make.short_description = 'Name'
+
 admin.site.register(User)
+
+# admin.site.register(Manufacturer)
--- apps/user/models.py
+++ apps/user/models.py
@@ -1,11 +1,21 @@
 from django.db import models
+from django.utils.html import format_html
+from django.conf import settings
+
+# class Manufacturer(models.Model):
+#     name = models.CharField(verbose_name=u'我的名字', max_length=5, null=True)
+#     age = models.TimeField(verbose_name=u'我的世界', null=True)
+#     def __str__(self):
+#     	return self.name
 
 class User(models.Model):
-	createtime = models.DateTimeField( u'创建时间', auto_now_add=True, null=True )
-	username = models.CharField( u'账号', max_length=20 )
-	password = models.CharField( u'密码', max_length=20 )
+	createtime = models.DateTimeField( verbose_name=u'创建时间', auto_now_add=True, null=True )
+	username = models.CharField( verbose_name=u'账号', max_length=20, unique=True, null=True )
+	password = models.CharField( verbose_name=u'密码', max_length=20, null=True )
 	def __str__(self):
 		return self.username
+	class Meta:
+		verbose_name = '前端用户'
+	
 		
-
 
